271648d455c3d903d47953ad1e62975ec2d645ae
Amir Motefaker <amotef@gmail.com>
2022-05-16 10:41:06 +0430

    Create image_contrast.py

---
+++ image_contrast.py
@@ -0,0 +1,27 @@
+# Code by amotef@gmail.com
+
+# Image Manipulation
+
+
+## Contrast
+
+from PIL import Image, ImageEnhance
+
+#read the image
+im = Image.open("image.png")
+
+#image brightness enhancer
+enhancer = ImageEnhance.Contrast(im)
+
+factor = 1 #gives original image
+im_output = enhancer.enhance(factor)
+im_output.save('image_contrast_original.png')
+
+factor = 0.5 #decrease constrast
+im_output = enhancer.enhance(factor)
+im_output.save('image_contrast_less.png')
+
+factor = 1.5 #increase contrast
+im_output = enhancer.enhance(factor)
+im_output.save('image_contrast_more.png')
+
