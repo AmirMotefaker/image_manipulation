8543c871ed5b524397febcebae286b8512bf4bc2
Amir Motefaker <amotef@gmail.com>
2022-05-16 10:28:47 +0430

    Create image_rotate.py

---
+++ image_rotate.py
@@ -0,0 +1,33 @@
+# Code by amotef@gmail.com
+
+# Image Manipulation
+
+
+## Rotate
+
+from PIL import Image, ImageDraw
+from math import sin, cos, pi
+
+
+# Load image:
+input_image = Image.open("image.png")
+input_pixels = input_image.load()
+
+# Create output image
+output_image = Image.new("RGB", input_image.size)
+draw = ImageDraw.Draw(output_image)
+
+angle = pi / 3  # angle in radian
+center_x = input_image.width / 2
+center_y = input_image.height / 2
+
+# Copy pixels
+for x in range(input_image.width):
+    for y in range(input_image.height):
+        # Compute coordinate in input image
+        xp = int((x - center_x) * cos(angle) - (y - center_y) * sin(angle) + center_x)
+        yp = int((x - center_x) * sin(angle) + (y - center_y) * cos(angle) + center_y)
+        if 0 <= xp < input_image.width and 0 <= yp < input_image.height:
+            draw.point((x, y), input_pixels[xp, yp])
+
+output_image.save("image_rotate_output.png")
