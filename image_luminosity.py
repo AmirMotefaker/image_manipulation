b5629cd9f755c0e2970654f05a9debe3c8f4da89
Amir Motefaker <amotef@gmail.com>
2022-05-16 10:36:49 +0430

    Create image_luminosity.py

---
+++ image_luminosity.py
@@ -0,0 +1,30 @@
+# Code by amotef@gmail.com
+
+# Image Manipulation
+
+
+## Luminosity
+
+from PIL import Image, ImageDraw
+
+# Change this:
+luminosity = 80
+
+# Load image:
+input_image = Image.open("image.png")
+input_pixels = input_image.load()
+
+# Create output image
+output_image = Image.new("RGB", input_image.size)
+draw = ImageDraw.Draw(output_image)
+
+# Generate image
+for x in range(output_image.width):
+    for y in range(output_image.height):
+        r, g, b = input_pixels[x, y]
+        r = int(r + luminosity)
+        g = int(g + luminosity)
+        b = int(b + luminosity)
+        draw.point((x, y), (r, g, b))
+
+output_image.save("image_luminosity_output.png")
